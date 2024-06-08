4506b8f3c84a084eee9016f64769955a55dcd03a
Guilherme Saud <gsaud1008@gmail.com>
2024-06-08 11:33:47 -0300

    feat: single decimal case grades
    feat: Included @validator inside Student model to round the grade to a single decimal case

--- app/models.py
+++ app/models.py
@@ -1,10 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import Dict
 
 class Student(BaseModel):
     id: int
     name: str
     grades: Dict[str, float]
+
+    @validator('grades', pre=True)
+    def round_grades(cls, grades):
+        return {subject: round(grade, 1) for subject, grade in grades.items()}
 
     def dict(self, **kwargs):
         return super().dict(**kwargs)
